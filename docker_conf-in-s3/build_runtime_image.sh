9c937f9cc921d4c20bb4c1e7620d6745d92ba506
Jason Umiker <jason.umiker@gmail.com>
2017-09-09 22:05:09 +1000

    Update build_runtime_image.sh

--- docker_conf-in-s3/build_runtime_image.sh
+++ docker_conf-in-s3/build_runtime_image.sh
@@ -1,2 +1,4 @@
 docker build -f Dockerfile-build -t shibbolth .
-echo "docker run --name=shibboleth -d -it --rm -p 8080:8080 shibtest run-jetty.sh" > run.sh
+echo "docker run --name=shibboleth -d -it --rm -p 8080:8080 shibboleth run-jetty.sh" > run.sh
+echo "docker run -d --name"nginx_redirect" --rm -p 8888:80 nginx_redirect" >> run.sh
+chmod +x run.sh
